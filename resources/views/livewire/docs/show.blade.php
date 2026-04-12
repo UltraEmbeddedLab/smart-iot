01468e8f0a82a503473ed2c05e854c0aecbfdaf5
Bogdan <gewaldb@gmail.com>
2026-04-12 23:31:32 +0200

    Expand maximum width of documentation container to `max-w-6xl` for better layout.

--- resources/views/livewire/docs/show.blade.php
+++ resources/views/livewire/docs/show.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="mx-auto w-full max-w-4xl px-4 py-6 sm:px-6 lg:px-8">
+    <div class="mx-auto w-full max-w-6xl px-4 py-6 sm:px-6 lg:px-8">
         <article
             x-data="{
                 copied: false,
